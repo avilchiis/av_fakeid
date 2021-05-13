f31015e72f837dfdc8a66e1239b83d5d3e8087f4
avilchiis <58269028+avilchiis@users.noreply.github.com>
2021-05-12 21:35:23 -0700

    Update fakeid.sql

--- fakeid.sql
+++ fakeid.sql
@@ -1,21 +1,6 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 CREATE TABLE IF NOT EXISTS `fakenames` (
   `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `firstname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
   `lastname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
   `dateofbirth` varchar(25) COLLATE utf8mb4_bin DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-/*!40000 ALTER TABLE `fakenames` DISABLE KEYS */;
-INSERT INTO `fakenames` (`identifier`, `firstname`, `lastname`, `dateofbirth`) VALUES
-	('steam:110000135585a76', 'Test', 'Two', '05/08/1975');
-/*!40000 ALTER TABLE `fakenames` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
